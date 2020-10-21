24d09d100833a6004b9bf18e5137676b6e5d5baf
Garry Prang <garryprang@grry>
2020-10-21 19:06:49 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan1/biodata/PBO210116147Latihan1Biodata.java
@@ -0,0 +1,31 @@
+package pbo2.pkg10116147.latihan1.biodata;
+/*
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk menampilkan biodata diri.
+ */
+public class PBO210116147Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("-------------------------------------------------");
+        System.out.println("BIODATA" + "\n" + "-------------------------------------------------");
+        System.out.println("NAMA\t\t:   Garry Prang");
+        System.out.println("ALAMAT\t\t:   Jl. Raden Patah No. 39");
+        System.out.println("TEMPAT LAHIR\t:   Manado");
+        System.out.println("TANGGAL LAHIR\t:   7 Desember 1998");
+        System.out.println("-------------------------------------------------");
+        System.out.println("HOBI\t\t:   Bermusik");
+        System.out.println("MAKANAN FAVORIT\t:   Ikan Bakar");
+        System.out.println("MINUMAN FAVORIT\t:   Kopi");
+        System.out.println("-------------------------------------------------");
+        
+        
+    }
+    
+}
